8de813adbebbd71a263fbc5721775e6d69729ddb
felics <felics6@gmail.com>
2024-07-16 00:17:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //// Task 1
+        System.out.println("Task 1");
+
+        Scanner read = new Scanner(System.in);
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        // System.out.print("Введите имя - ");
+        // String firstName = read.next();
+
+        // System.out.print("Введите отчество ");
+        // String middleName = read.next();
+
+        // System.out.print("Введите фамилию ");
+        // String lastName = read.next();
+
+        // String fullName = "Ф. И. О. сотрудника — " + lastName + " " + firstName + " " + middleName;
+
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        read.close();
+
+        //// Task 2
+        System.out.println();
+        System.out.println("Task 2");
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //// Task 3
+        System.out.println();
+        System.out.println("Task 3");
+
+    }
+}
